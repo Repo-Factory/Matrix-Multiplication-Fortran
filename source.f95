9b60c38f58a058bef31c1077d21b0dbcbdd8be6e
ConnerS <conner.sommmerfield@gmail.com>
2023-03-05 16:57:07 -0800

    Matrices now populating correctly

--- source.f95
+++ source.f95
@@ -1,56 +1,53 @@
-function read_matrix(filename) result(matrix)
+module matrices
     implicit none
-    
-    ! input/output
-    character(len=*), intent(in)        :: filename
-    real, dimension(:,:)   :: matrix
-    
-    integer :: x                     ! temp storage
-    integer :: i, j                  ! loop variables
-    integer :: rows, columns
-    
-    ! open file, read first line with rows/columns, allocate matrix
-    open(10, file=filename)
-    read(10, *) rows, columns
-    allocate(matrix(rows,columns))
-    
-    ! nested loop to fill matrix
-    do i = 1, rows
-        do j = 1, columns
-            read(10, *) x
-            matrix(i,j) = x
-        end do
-    end do
-    
-    close(10)
-end function read_matrix
-    
-    
-program multMatrix
-    implicit none
-    
-    real, dimension(:,:) :: matrix1, matrix2
-    integer :: i, j
-    integer :: rows1, columns1, rows2, columns2
-    
-    matrix1 = read_matrix("file1.txt")
-    matrix2 = read_matrix("file2.txt")
-    
-    rows1 = size(matrix1, 1)
-    columns1 = size(matrix1, 2)
-    rows2 = size(matrix2, 1)
-    columns2 = size(matrix2, 2)
-    
-    do i = 1, rows1
-        do j = 1, columns1
-            print *, matrix1(i, j)
-        end do
-    end do
-    
-    do i = 1, rows2
-        do j = 1, columns2
-            print *, matrix2(i, j)
-        end do
-    end do
+    contains
 
+    subroutine populate_matrix(filename, matrix, rows, columns)
+        character(*), intent(in) :: filename
+        integer, intent(in) :: rows, columns
+        real, intent(out) :: matrix(:,:)
+        integer :: i, j
+        open(10, file=filename)
+        do i=1, rows
+            do j=1, columns
+                read(10, *) matrix(i,j)
+            end do
+        end do
+        close(10)
+    end subroutine populate_matrix
+
+    subroutine prompt_dimensions(rows, columns)
+        integer, intent(out) :: rows, columns
+        print *, "Please type number of rows"
+        read *, rows
+        print *, "Please type number of columns"
+        read *, columns
+    end subroutine prompt_dimensions
+end module matrices
+
+
+
+program multMatrix
+    use matrices
+    implicit none
+
+    real, allocatable :: matrix(:,:)
+    integer :: rows, columns
+    integer :: i, j
+
+    call prompt_dimensions(rows, columns)
+    allocate(matrix(rows, columns))
+    
+    call populate_matrix("matrix1.txt", matrix, rows, columns)
+
+    do i=1, rows
+        do j=1, columns
+            if (j == columns) then
+                write(*, fmt="(1x,f7.2)", advance="yes") matrix(i,j)
+            else
+                write(*, fmt="(1x,f7.2)", advance="no") matrix(i,j)
+            endif
+        end do
+    end do
+    
 end program multMatrix
